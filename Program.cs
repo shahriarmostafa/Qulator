f4593b3dff0cef6189cbf4a2b3354245a2e49cb8
shahriarmostafa <sahirmiashab@gmail.com>
2026-01-17 02:50:41 +0600

    done

--- Program.cs
+++ Program.cs
@@ -8,19 +8,14 @@ namespace Sim1Test
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Login());
-            //Application.Run(new Registration());
-            //Application.Run(new SimulationPage());
-            Application.Run(new WeatherForm());
-
+            Application.Run(new Registration());
         }
     }
 }
